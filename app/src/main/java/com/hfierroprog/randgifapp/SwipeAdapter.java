7a2e0c7ba862ad8389e0af36cf2bbd9b137d524e
root <hfierroprog@gmail.com>
2018-01-14 17:58:04 -0300

    Se agrega la clase SwipeAdapter

---
+++ app/src/main/java/com/hfierroprog/randgifapp/SwipeAdapter.java
@@ -0,0 +1,29 @@
+package com.hfierroprog.randgifapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class SwipeAdapter extends FragmentStatePagerAdapter{
+
+    public SwipeAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new Fragment();
+        //Mandamos la posicion mediante bundle
+        Bundle bundle = new Bundle();
+        bundle.putInt("count",position);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 99999;
+    }
+}
