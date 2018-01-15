56ae5e9cf7802848d83a88db9cd2bd94cd313ccf
root <hfierroprog@gmail.com>
2018-01-15 16:03:25 -0300

    Se obtiene enlace del gif desde JSON remoto

--- app/src/main/java/com/hfierroprog/randgifapp/PageFragment.java
+++ app/src/main/java/com/hfierroprog/randgifapp/PageFragment.java
@@ -10,6 +10,9 @@ import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class PageFragment extends android.support.v4.app.Fragment{
 
     private View view;
@@ -26,12 +29,24 @@ public class PageFragment extends android.support.v4.app.Fragment{
         Ion.with(getContext())
                 .load("https://api.giphy.com/v1/gifs/random?api_key=UTvzNeIbjkDQnzhmx3PwFrMZVT6INkfI&tag=&rating=G")
                 .asJsonObject()
-                .setCallback(new FutureCallback<JsonObject>(){
+                .setCallback(new FutureCallback<JsonObject>() {
                     @Override
                     public void onCompleted(Exception e, JsonObject result) {
-                        text.setText(result.toString());
+
+                        JsonObject jsonObject = result.getAsJsonObject("data");
+
+                        try {
+                            JSONObject JSON = new JSONObject(jsonObject.toString());
+
+                            text.setText(JSON.getString("image_original_url"));
+                        } catch (JSONException e1) {
+                            e1.printStackTrace();
+                        }
+
+
                     }
                 });
+
 
         return view;
     }
