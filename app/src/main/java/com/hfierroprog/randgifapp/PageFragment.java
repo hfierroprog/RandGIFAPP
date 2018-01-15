8888189f507fb27b43018937849368af52ef3e5d
root <hfierroprog@gmail.com>
2018-01-15 17:08:40 -0300

    Se completa la funcionalidad deseada
    Se ha agregado la libreria glide que se encarga de obtener el gif mediante
    el link y la pasa a la vista

--- app/src/main/java/com/hfierroprog/randgifapp/PageFragment.java
+++ app/src/main/java/com/hfierroprog/randgifapp/PageFragment.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.gson.JsonObject;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
@@ -16,15 +18,14 @@ import org.json.JSONObject;
 public class PageFragment extends android.support.v4.app.Fragment{
 
     private View view;
-    private TextView text;
+    private ImageView imgGIF;
 
     @Override
     public View onCreateView(LayoutInflater inflater, final ViewGroup container, Bundle savedInstanceState)
     {
         view = inflater.inflate(R.layout.page_fragment_layout, container, false);
 
-        text = view.findViewById(R.id.txtEdit);
-
+        imgGIF = view.findViewById(R.id.imgGIF);
 
         Ion.with(getContext())
                 .load("https://api.giphy.com/v1/gifs/random?api_key=UTvzNeIbjkDQnzhmx3PwFrMZVT6INkfI&tag=&rating=G")
@@ -38,12 +39,20 @@ public class PageFragment extends android.support.v4.app.Fragment{
                         try {
                             JSONObject JSON = new JSONObject(jsonObject.toString());
 
-                            text.setText(JSON.getString("image_original_url"));
+                            String link = JSON.getString("fixed_height_small_url");
+
+
+                            Glide
+                                    .with(getContext())
+                                    .load(link)
+                                    .fitCenter()
+                                    .diskCacheStrategy(DiskCacheStrategy.SOURCE)
+                                    .into(imgGIF);
+
+
                         } catch (JSONException e1) {
                             e1.printStackTrace();
                         }
-
-
                     }
                 });
 
